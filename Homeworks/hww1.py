44f74d90dfe353f52362787946963009ab4118fa
Emma Pham <143017877+EMMAPHA@users.noreply.github.com>
2025-03-26 11:17:05 -0500

    Add files via upload

---
+++ Homeworks/hww1.py
@@ -0,0 +1,18 @@
+""" def rec_pow(x, n):
+    if n == 0:
+        return 1
+    else:
+        return x * rec_pow(x, n - 1) """
+        
+        
+""" function iterativeExp(x, n):
+    result = 1
+    while n > 0:
+        if n is odd:
+            result *= x
+           
+        x *= x
+        n = floor(n / 2)
+    return result 3 """
+    
+    
